2e253f0d04b7744132ae9e8b55d04f411bf1973c
Caston Young <castony.pa@gmail.com>
2015-10-12 02:04:28 -0700

    No new code added. Added comments to obey cset standards. Deleted debug code on Fill function. Separated Exception and display code, by adding a new .cpp file.
    Again no new code was added.

--- Two Dimensional Arrays/Two Dimensional Arrays/Array_Template.h
+++ Two Dimensional Arrays/Two Dimensional Arrays/Array_Template.h
@@ -10,53 +10,64 @@
 #include"Exceptions.h"
 
 TP
-class Row;
+class cRow;//forward declaration
 
-/*A Two Dimensional Array handler*/
-TP
-class Array
+TP/*A Dynamic Two Dimensional Array handler,
+  Allows you to use dynamic 2D arrays, just like regular 2D arrays,
+  also allows assignment, and copying by constructor.
+Enter	: Nothing | a row and collumn length | another cArray
+Exit	: One possible exception, and one allowocation error, I failed to get rid of in time.*/
+class cArray
 {public:
-	 Array();									//Primary Const
-	 Array(int row, int col = 0);				//Seeded array Const
-	 Array(const Array& copy);					//Copy Const
-	~Array();
-	 Array&	operator= (const Array& rhs);		//merge two arrays.
-	 Row<T>	operator[](int index);				//Return value from array
-	 T&   getCel(int row, int col);				//The lab over seer said I wouldn't loose points for this
+	 cArray();									//Primary Const
+	 cArray(int row, int col = 0);				//Seeded array Const
+	 cArray(const cArray& copy);				//Copy Const
+	~cArray();
+	 cArray&operator= (const cArray& rhs);		//merge two arrays.
+	 cRow<T>	operator[](int index);			//Return value from array
+	 T&   getCel(int row, int col);				//return address of a var from the array
 	 int  getRow()			{ return m_row; }
 	 int  getCol()			{ return m_col; }
 	 void setRow(int rows)	{ m_row = rows; }
 	 void setCol(int cols)	{ m_col = cols; }
 private:
 	T**	m_array;								//The Array
-	int m_row, m_col, i;
+	int m_row, m_col, i;	//the length of a row, the length of a column, and an index.
 };
 
-TP/*A class made to support dual brace operators*/
-class Row
+TP/*A class made to support dual brace operators, it is only used once by cArray*/
+class cRow
 {public:
-	Row(Array<T>& array_, int row) : Row_array(array_), Row_row(row){}	//Initialization
+	cRow(cArray<T>& array_, int row) : Row_array(array_), Row_row(row){}	//Initialization
 	T& operator[](int column){	return Row_array.getCel(Row_row, column);	}//secondary brace op.
 private:
-	Array<T>& Row_array;
+	cArray<T>& Row_array;
 	int Row_row;
 };
 
-TP//default constructor.
-Array<T>::Array()//Primary Const
+TP/*Default Constructor
+Enter	: Nothing
+Return	: Nothing*/
+cArray<T>::cArray()//Primary Const
 {	m_array		= new T*[1];											//make dynamic,
 	m_array[0]	= new T [1];											//set to 2 to allow terminating char
 	m_array[0][0] = NULL;												//Terminate
 }
 
-TP//Seeded array Const
-Array<T>::Array(int row, int col = 0) : m_row(row), m_col(col)			//initialize
+TP/*"seeded" (not default or copy) constructor
+  Creates a new cArray with a row and column length as specified
+Enter	: row, and possibly column length.
+		  (if no column length is specified it defaults to 0)
+Return	: Nothing*/
+cArray<T>::cArray(int row, int col = 0) : m_row(row), m_col(col)		//initialization
 {	m_array = new T*[m_row];											//make dynamic
-	FOR(m_row) m_array[i] = new T[m_col];
+	FOR(m_row) m_array[i] = new T[m_col];								//copy data
 }
 
-TP//Copy Const
-Array<T>::Array(const Array& copy)
+TP/*A copy constructor (creates a deep copy of another cArray)
+Enter	: ranother cArray
+Return	: Nothing*/
+cArray<T>::cArray(const cArray& copy)
 {	m_row = copy.m_row;
 	m_col = copy.m_col;
 	m_array = new T*[m_row];
@@ -66,28 +77,34 @@ Array<T>::Array(const Array& copy)
 	}
 }
 
-TP
-Array<T>::~Array()	{  }
+TP/*Destructor, takes and returns nothing*/
+cArray<T>::~cArray()	{  }
 
-TP
-T& Array<T>::getCel(int row, int col)	{ return m_array[row][col]; }
+TP/*Required by the lab guy, he said no points would because of it.
+Enter	: row, and column coordinate.
+Return	: the address of said slot.*/
+T& cArray<T>::getCel(int row, int col)	{ return m_array[row][col]; }
 
-TP//copy
-Array<T>& Array<T>::operator=(const Array& rhs)
+TP/*Assignment operator, does a deep copy
+Enter	: another cArray
+		  m_row, m_col, and all data will be copied.
+Return	: *this*/
+cArray<T>& cArray<T>::operator=(const cArray& rhs)
 {	m_row = rhs.m_row;
 	m_col = rhs.m_col;
 	FOR(m_row) memcpy(m_array[i], rhs.m_array[i], m_col); 				//copy array
 	return *this;
 }
 
-TP//Return value from array
-Row<T> Array<T>::operator[](int index)
+TP/*Return a value from the array cArray (X[i])[j]
+Enter	: an index
+Return	: a_row
+Exit	: this will cause cArray[i][j] to become a_row[j]
+		  If handled improprerly an exception might be thrown.*/
+cRow<T> cArray<T>::operator[](int index)
 {	if(index >= 0 && index <= m_row)
-	{	
-		Row<T> a_row(*this, index);
+	{	cRow<T> a_row(*this, index);
 		return a_row;
-	}
-	else throw new Exception("Error Out of Bounds.\n");	
+	}else throw new Exception("Error Out of Bounds.\n");	
 }
-//I'd put more comments in but I lack the time.
 #endif
--- Two Dimensional Arrays/Two Dimensional Arrays/Source.cpp
+++ Two Dimensional Arrays/Two Dimensional Arrays/Source.cpp
@@ -1,36 +1,49 @@
+/*		  Two Dimensional Arrays
+ *		Caston Young	10/11/2015
+ *				  CST 211
+ *		Files: Array_Template.h
+ *				Exceptions.h
+ *				Source.cpp
+ *				Board.cpp
+ *		Input: None.	Output: None.
+ */
+
+/*Source.cpp contains main function, and Fill, both of  which
+  are designed to test the code from the other files*/
+
 #include<iostream>
 #include"Array_Template.h"
 using namespace std;
 
-extern void Display_Board(Array<char> &DB_2D);
+extern void Display_Board(cArray<char> &DB_2D);
 
-inline void Fill(Array<char>& F_2D, char c)
+//Fills a 2D character array with a given character
+inline void Fill(cArray<char>& F_2D, char c)
 {	int i, j;
 	FOR(F_2D.getRow())
 		for(j=0; j < F_2D.getCol(); j++){
-			if ( i == 3 && j == 0)
-				{ void* break_point; }
-			F_2D[i][j] = c;			//Code breaks upon assignment of F_D2[3][0]
-			if (j==0) cout<< endl;
-			cout<< F_2D[i][j];
+			if ( i == 3 && j == 0)	//<V Stops for break point.
+				{ void* insert_break_point; }
+			F_2D[i][j] = c;			//Problematic assignment of F_D2[F_D2+1][0]
 		}
-}	//D2[i][j] = '\n';				//Not needed for current display method.
+}
 
+//Test Classes
 int main()
 {try{
 	int M_row = WIDTH, M_col = HEIGH;
 	int i, j;//Index numbers
-	Array<char> M_2D(M_row, M_col);
+	cArray<char> M_2D(M_row, M_col);
 
 	//cin >> M_row >> M_col;
 	Fill(M_2D, '#');
 	Display_Board(M_2D);
-	Array<char> M_2D2(M_2D);
+	cArray<char> M_2D2(M_2D);
 
 	//cout<< "Re-enter:";
 	//cin >> M_row >> M_col;
-	M_row = 5;	M_col = 3;
-	M_2D.setRow(M_row);		M_2D.setCol(M_col);
+	M_row = 5;			M_col = 3;
+	M_2D.setRow(M_row);	M_2D.setCol(M_col);
 	Fill(M_2D, '$');
 
 	Display_Board(M_2D2);
