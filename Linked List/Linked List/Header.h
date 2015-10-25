849b3f09451e95eeb2f455c4a76d14096f2edf4a
Caston Young <castony.pa@gmail.com>
2015-10-24 23:59:08 -0700

    Saturday hand in.

--- Linked List/Linked List/Header.h
+++ Linked List/Linked List/Header.h
@@ -20,49 +20,15 @@ bool operator==(sub d1, string s2)	{ return (d1.name == s2)	 ; }
 
 class Cell
 {public:
-	sub cell_ID;//Pound define this.
-	string* airport;
-	int m_data;
+	Cell();
+	Cell(sub cell);
 
-	Cell()
-	{	cout<< "Enter City(Name ST number code1 code2 ...)\n";
-		cin >> cell_ID.name >> cell_ID.state >> cell_ID.N_ports;
-		airport = new string[/*N_ports*/ 10];
-		for(int N=0; N < cell_ID.N_ports; N++) cin >> airport[N];//Ask for airport codes
-	}
-	Cell(sub cell) : cell_ID(cell)
-	{	airport = new string[/*N_ports*/ 10];
-		for(int N=0; N < cell_ID.N_ports; N++) cin >> airport[N];//Ask for airport codes
-	}
+	sub GetID()			{ return cell_ID; }
+	string* Get_port()	{ return airport; }
+	int Get_data()		{ return m_data;  }
 
-	void Add_Airport(string cOd)//Adds an airport
-	{	airport[++cell_ID.N_ports-1] = cOd;
-		/*new_code = new string[N_ports+1];
-		for(int N=0; N < N_ports; N++) new_code[N] = airport[N];
-		new_code[++N_ports-1] = cOd;
-		airport = new_code;
-		//delete [] new_code;//Will this erase the data?*/
-	}
-
-	bool Delete_Airport(string cOd)//Deletes an airport.
-	{	/*int N;
-		new_code = new string[N_ports-1];
-		for(N=0; N < N_ports; N++)	{ if(cOd!=airport[N]) new_code[N] = airport[N]; else break; }
-		if(N < N_ports){
-			while(++N < N_ports) new_code[N-1] = airport[N];
-			--N_ports;
-			airport = new_code;
-			//delete [] new_code;//<Will this erase the data?
-		} else { return true;	/*delete [] new_code;*/ //}//<J
-		int N;
-		for(N=0; N < cell_ID.N_ports; N++)	if (cOd==airport[N]) break;	//	If the bad airport is found stop this loop 
-		if(N < cell_ID.N_ports){										//	,and
-			while(++N < cell_ID.N_ports) airport[N-1] = airport[N];		//	Start this loop, which shifts the data.
-			--cell_ID.N_ports;
-		} else return true;							//Return an error bit if cOd was not found.
-		return false;
-	}
-
+	void Add_Airport	(string cOd);
+	bool Delete_Airport (string cOd);
 	void Display();
 
 	/*~Cell()
@@ -70,7 +36,11 @@ class Cell
 			delete [] airport;
 			airport = 0;
 	} /*deceptive*/ //}
-	
-};//But looks nice
+	//But looks nice
+private:
+	sub cell_ID;//Pound define this.
+	string* airport;
+	int m_data;
+};
 
 #endif
